28d499b89c1403d5aed985da667a2eba264c1ce4
Titan Yuan <tryuan99@gmail.com>
2023-06-21 12:45:48 -0700

    Add complex LASSO model

--- utils/optimization/linear_model.py
+++ utils/optimization/linear_model.py
@@ -2,6 +2,7 @@
 
 from abc import ABC, abstractmethod
 
+import cvxpy as cp
 import numpy as np
 from sklearn import linear_model
 
@@ -63,3 +64,51 @@ class LassoModel(LinearModel):
     def get_coefficients(self) -> np.ndarray:
         """Returns the coefficients of the best linear fit."""
         return self.model.coef_
+
+
+class ComplexLassoModel(LinearModel):
+    """Linear model with L1 regularization (LASSO) that supports complex values.
+
+    The optimization objective is:
+    (1 / (2 * num_samples)) * ||y - Xw||_2^2 + alpha * ||w||_1.
+
+    To support complex values, we split all matrices and vectors into real and
+    imaginary components and formulate the problem as a second-order cone problem.
+    See https://stats.stackexchange.com/a/469660 for more details.
+    """
+
+    def __init__(self, X: np.ndarray, y: np.ndarray, alpha: float):
+        super().__init__(X, y)
+        # Number of features.
+        self.n = self.X.shape[-1]
+        # X is a block matrix of dimensions (2 * number of samples) x (2 *
+        # number of features).
+        self.X = np.block([
+            [np.real(self.X), -np.imag(self.X)],
+            [np.imag(self.X), np.real(self.X)],
+        ])
+        # y has dimensions (2 * number of samples) x 1.
+        self.y = np.block([np.real(self.y), np.imag(self.y)])
+
+        # The imaginary component is stacked below the real component, so w has
+        # dimensions (2 * number of features) x 1.
+        self.w = cp.Variable(2 * self.n)
+        # There is one slack variable for each feature.
+        self.t = cp.Variable(self.n)
+        constraints = [
+            # |w_k| = sqrt(wr_k^2 + wi_k^2) = ||[wr_k \\ wi_k]||_2 <= t_k.
+            cp.SOC(self.t[k], cp.vstack((self.w[k], self.w[k + self.n])))
+            for k in range(self.n)
+        ]
+        self.problem = cp.Problem(
+            cp.Minimize(1 / (2 * self.n) *
+                        cp.sum_squares(self.y - self.X @ self.w) +
+                        alpha * cp.sum(self.t)), constraints)
+
+    def solve(self) -> None:
+        """Solves the linear model with L1 regularization (LASSO)."""
+        self.problem.solve()
+
+    def get_coefficients(self) -> np.ndarray:
+        """Returns the coefficients of the best linear fit."""
+        return self.w.value[:self.n] + 1j * self.w.value[self.n:]
--- utils/optimization/linear_model_test.py
+++ utils/optimization/linear_model_test.py
@@ -1,7 +1,8 @@
 import numpy as np
 from absl.testing import absltest
 
-from utils.optimization.linear_model import LassoModel, LinearRegression
+from utils.optimization.linear_model import (ComplexLassoModel, LassoModel,
+                                             LinearRegression)
 
 
 class LinearModelTestCase(absltest.TestCase):
@@ -40,6 +41,46 @@ class LassoRegressionTestCase(LinearModelTestCase):
             np.testing.assert_allclose(lasso_model.get_coefficients(),
                                        np.array([-1.988821, 0.982232]),
                                        atol=1e-6))
+
+
+class ComplexLassoRegressionTestCase(LassoRegressionTestCase):
+
+    def test_fit_real_line(self):
+        lasso_model = ComplexLassoModel(self.X, self.y, 0.01)
+        lasso_model.solve()
+        self.assertIsNone(
+            np.testing.assert_allclose(lasso_model.get_coefficients(),
+                                       np.array([-1.9, 0.84]),
+                                       atol=1e-5))
+
+        lasso_model = ComplexLassoModel(self.X, self.y, 0.001)
+        lasso_model.solve()
+        self.assertIsNone(
+            np.testing.assert_allclose(lasso_model.get_coefficients(),
+                                       np.array([-1.99, 0.984]),
+                                       atol=1e-5))
+
+    def test_fit_complex_line(self):
+        X = np.array([
+            [1, 1j],
+            [2, 1j],
+        ])
+        y = np.array([2 - 1j, 3])
+
+        lasso_model = ComplexLassoModel(X, y, 0.01)
+        lasso_model.solve()
+        self.assertIsNone(
+            np.testing.assert_allclose(lasso_model.get_coefficients(),
+                                       np.array(
+                                           [0.9988 + 0.92j, -1.871 - 0.9971j]),
+                                       atol=1e-4))
+
+        lasso_model = ComplexLassoModel(X, y, 1e-6)
+        lasso_model.solve()
+        self.assertIsNone(
+            np.testing.assert_allclose(lasso_model.get_coefficients(),
+                                       np.array([1 + 1j, -2 - 1j]),
+                                       atol=1e-4))
 
 
 if __name__ == "__main__":
