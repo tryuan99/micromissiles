e378450df78a315c4ff10242ff7114912c466cab
Titan Yuan <tryuan99@gmail.com>
2023-06-30 14:31:11 -0700

    Plot nonlinear chirp samples as non-uniform samples

---
+++ simulation/radar/sandbox/non_uniform_sampling_main.py
@@ -0,0 +1,52 @@
+"""Explores non-uniform sampling."""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from absl import app
+
+
+def _generate_quadratic_chirp(t_axis: np.ndarray, a: float) -> np.ndarray:
+    """Generates a quadratic chirp.
+
+    Args:
+        t_axis: Time axis.
+        a: Quadratic coefficient.
+
+    Returns:
+        The samples of a quadratic chirp along the given time axis.
+    """
+    return np.exp(1j * 2 * np.pi * a * t_axis**2)
+
+
+def plot_non_uniform_sampling_quadratic() -> None:
+    """Plots the quadratically uniform samples as non-uniform samples."""
+    a = 560
+    LENGTH = 8
+    fs = 100  # Hz
+    t = np.linspace(0, LENGTH / fs, 10000)
+    x = _generate_quadratic_chirp(t, a)
+    n = np.arange(LENGTH) / fs
+    samples = _generate_quadratic_chirp(n, a)
+
+    fig, ax = plt.subplots(1, 2, figsize=(16, 10), sharey=True)
+    ax[0].plot(t, np.real(x))
+    ax[0].stem(n, np.real(samples), "r")
+    ax[0].set_xlabel("Time in s")
+    ax[0].set_title("Uniform samples of nonlinear chirp")
+
+    t_non_uniform = np.linspace(0, (LENGTH / fs)**2, 10000)
+    x_non_uniform = _generate_quadratic_chirp(np.sqrt(t_non_uniform), a)
+    ax[1].plot(t_non_uniform, np.real(x_non_uniform))
+    ax[1].stem(n**2, np.real(samples), "r")
+    ax[1].set_xlabel("Time in s")
+    ax[1].set_title("Non-uniform samples of a sinusoid")
+    plt.show()
+
+
+def main(argv):
+    assert len(argv) == 1, argv
+    plot_non_uniform_sampling_quadratic()
+
+
+if __name__ == "__main__":
+    app.run(main)
