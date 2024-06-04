e4915c49b49c76457582438b8b5301981bcc2c7e
Titan Yuan <tryuan99@gmail.com>
2024-06-03 23:59:04 -0700

    Validate the radar subframe data header prior to parsing and the TLVs

--- radar/ti/ti_radar_subframe_data_aggregator.py
+++ radar/ti/ti_radar_subframe_data_aggregator.py
@@ -9,7 +9,8 @@ from absl import logging
 
 from radar.ti.ti_radar_data_handler import TiRadarDataHandler
 from radar.ti.ti_radar_subframe_data import (
-    TI_RADAR_SUBFRAME_DATA_HEADER_MAGIC_WORD, TiRadarSubframeDataHeader)
+    TI_RADAR_SUBFRAME_DATA_HEADER_MAGIC_WORD,
+    TI_RADAR_SUBFRAME_MAX_NUM_DETECTED_OBJECTS, TiRadarSubframeDataHeader)
 from radar.ti.ti_radar_subframe_data_handler import TiRadarSubframeDataHandler
 from radar.ti.ti_radar_subframe_data_parser import TiRadarSubframeDataParser
 
@@ -26,8 +27,7 @@ class TiRadarSubframeDataAggregator(TiRadarDataHandler):
     """TI radar subframe data aggregator."""
 
     def __init__(self) -> None:
-        self.state = TiRadarSubframeDataAggregatorState.WAITING_FOR_HEADER
-        self.buffer = bytearray()
+        self._reset_for_next_subframe()
         self.remaining_header_length = 0
         self.remaining_subframe_data_length = 0
         self.subframe_data_handlers: list[TiRadarSubframeDataHandler] = []
@@ -56,7 +56,7 @@ class TiRadarSubframeDataAggregator(TiRadarDataHandler):
                     # Look for the magic word in the header.
                     if not TI_RADAR_SUBFRAME_DATA_HEADER_MAGIC_WORD.startswith(
                             self.buffer):
-                        self.buffer = bytearray()
+                        self._reset_for_next_subframe()
                     elif self.buffer == TI_RADAR_SUBFRAME_DATA_HEADER_MAGIC_WORD:
                         # The magic word was received.
                         self.state = (
@@ -71,12 +71,18 @@ class TiRadarSubframeDataAggregator(TiRadarDataHandler):
                         self.remaining_subframe_data_length = (
                             header.get("packet_length") -
                             TiRadarSubframeDataHeader.size())
-                        if self.remaining_subframe_data_length > 0:
-                            self.state = (TiRadarSubframeDataAggregatorState.
-                                          RECEIVING_SUBFRAME_DATA)
+                        try:
+                            self._validate_header(header)
+                        except Exception as e:
+                            logging.error("Failed to validate header: %s.", e)
+                            self._reset_for_the_next_subframe()
                         else:
-                            self.state = (TiRadarSubframeDataAggregatorState.
-                                          FINISHED_SUBFRAME)
+                            if self.remaining_subframe_data_length > 0:
+                                self.state = (TiRadarSubframeDataAggregatorState
+                                              .RECEIVING_SUBFRAME_DATA)
+                            else:
+                                self.state = (TiRadarSubframeDataAggregatorState
+                                              .FINISHED_SUBFRAME)
                 case TiRadarSubframeDataAggregatorState.RECEIVING_SUBFRAME_DATA:
                     if self.remaining_subframe_data_length == 0:
                         # The entire subframe data was received.
@@ -85,18 +91,56 @@ class TiRadarSubframeDataAggregator(TiRadarDataHandler):
 
             if self.state == TiRadarSubframeDataAggregatorState.FINISHED_SUBFRAME:
                 # Parse the subframe data.
-                try:
-                    parser = TiRadarSubframeDataParser(self.buffer)
-                except Exception as e:
-                    logging.error("Failed to parse radar subframe data: %s.", e)
-                else:
-                    subframe_data = parser.subframe_data
+                parser = TiRadarSubframeDataParser(self.buffer)
+                subframe_data = parser.subframe_data
 
-                    # Handle the subframe data.
-                    # TODO(titan): This can be multithreaded for better performance.
-                    for handler in self.subframe_data_handlers:
-                        handler.handle_subframe_data(subframe_data)
+                # Handle the subframe data.
+                # TODO(titan): This can be multithreaded for better performance.
+                for handler in self.subframe_data_handlers:
+                    handler.handle_subframe_data(subframe_data)
 
                 # Reset for the next subframe.
-                self.buffer = bytearray()
-                self.state = TiRadarSubframeDataAggregatorState.WAITING_FOR_HEADER
+                self._reset_for_next_subframe()
+
+    def _reset_for_next_subframe(self) -> None:
+        """Resets for the next subframe."""
+        self.buffer = bytearray()
+        self.state = TiRadarSubframeDataAggregatorState.WAITING_FOR_HEADER
+
+    @staticmethod
+    def _validate_header(header: TiRadarSubframeDataHeader) -> None:
+        """Validates the radar subframe data header.
+
+        Raises:
+            ValueError: If the header fails validation.
+        """
+        # Maximum packet length in bytes.
+        MAX_PACKET_LENGTH = 921600 / 8
+
+        # Maximum number of TLVs.
+        MAX_NUM_TLVS = 8
+
+        # Maximum subframe number.
+        MAX_SUBFRAME_NUMBER = 10
+
+        packet_length = header.get("packet_length")
+        if packet_length > MAX_PACKET_LENGTH:
+            raise ValueError(
+                f"Packet length of {packet_length} exceeds the maximum packet "
+                f"length of {MAX_PACKET_LENGTH}.")
+        num_detected_objects = header.get("num_detected_objects")
+        if num_detected_objects > TI_RADAR_SUBFRAME_MAX_NUM_DETECTED_OBJECTS:
+            raise ValueError(
+                f"Number of detected objects {num_detected_objects} exceeds "
+                f"the maximum number of detected objects "
+                f"{TI_RADAR_SUBFRAME_MAX_NUM_DETECTED_OBJECTS}.")
+        num_tlvs = header.get("num_tlvs")
+        if num_tlvs > MAX_NUM_TLVS:
+            raise ValueError(
+                f"Number of TLVs {num_tlvs} exceeds the maximum number of TLVs "
+                f"{MAX_NUM_TLVS}.")
+        subframe_number = header.get("subframe_number")
+        if subframe_number > MAX_SUBFRAME_NUMBER:
+            raise ValueError(
+                f"Subframe number {subframe_number} exceeds the maximum "
+                f"subframe number {MAX_SUBFRAME_NUMBER}.")
--- radar/ti/ti_radar_subframe_data_parser.py
+++ radar/ti/ti_radar_subframe_data_parser.py
@@ -2,6 +2,8 @@
 radar subframe to the individual radar subframe data types.
 """
 
+from absl import logging
+
 from radar.ti.ti_radar_subframe_data import (TiRadarSubframeData,
                                              TiRadarSubframeDataHeader,
                                              TiRadarSubframeDataType,
@@ -24,7 +26,7 @@ class TiRadarSubframeDataParser:
             data: Bytes to parse.
 
         Returns:
-            The parser radar subframe data.
+            The parsed radar subframe data.
         """
         subframe_data = TiRadarSubframeData()
 
@@ -42,6 +44,25 @@ class TiRadarSubframeDataParser:
             index += TiRadarSubframeDataTypeLength.size()
             data_type = data_type_length.get("type")
             length = data_type_length.get("length")
+
+            # Validate the data type.
+            # TODO(titan): In Python 3.12, use the in statement.
+            try:
+                TiRadarSubframeDataType(data_type)
+            except ValueError:
+                logging.warning("TLV contains an invalid data type: %u.",
+                                data_type)
+                break
+            if (TiRadarSubframeDataType(data_type) ==
+                    TiRadarSubframeDataType.HEADER):
+                logging.warning("TLV contains an invalid data type: %u.",
+                                data_type)
+                break
+
+            # Validate the length.
+            if index + length > len(data):
+                logging.warning("TLV contains an invalid length: %u.", length)
+                break
 
             # Parse the corresponding data.
             subframe_data.add_data(TiRadarSubframeDataType(data_type),
