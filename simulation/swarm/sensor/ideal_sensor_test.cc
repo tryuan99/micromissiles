55b1a6d5277f9605cd40967273af8d574c94985e
Titan Yuan <tryuan99@gmail.com>
2024-09-10 00:13:13 -0700

    Add parameterized tests for ideal sensor

--- simulation/swarm/sensor/ideal_sensor_test.cc
+++ simulation/swarm/sensor/ideal_sensor_test.cc
@@ -3,9 +3,9 @@
 #include <gtest/gtest.h>
 
 #include <Eigen/Dense>
+#include <numbers>
 
 #include "simulation/swarm/model_agent.h"
-#include "simulation/swarm/proto/agent.pb.h"
 #include "simulation/swarm/proto/state.pb.h"
 
 namespace swarm::sensor {
@@ -14,33 +14,62 @@ namespace {
 // Maximum error tolerance.
 constexpr double kMaxErrorTolerance = 1e-6;
 
-class IdealSensorTest : public testing::Test {
+// Test parameter struct.
+struct IdealSensorTestParam {
+  // Agent position.
+  Eigen::Vector3d agent_position;
+
+  // Agent velocity.
+  Eigen::Vector3d agent_velocity;
+
+  // Target position.
+  Eigen::Vector3d target_position;
+
+  // Target velocity.
+  Eigen::Vector3d target_velocity;
+
+  // Expected range.
+  double expected_range = 0;
+
+  // Expected azimuth.
+  double expected_azimuth = 0;
+
+  // Expected elevation.
+  double expected_elevation = 0;
+
+  // Expected range rate.
+  double expected_range_rate = 0;
+
+  // Expected azimuth velocity.
+  double expected_azimuth_velocity = 0;
+
+  // Expected elevation velocity.
+  double expected_elevation_velocity = 0;
+};
+
+class IdealSensorTest : public testing::TestWithParam<IdealSensorTestParam> {
  protected:
-  // Agent state.
-  const Eigen::Vector3d kAgentPosition{0, 0, 0};
-  const Eigen::Vector3d kAgentVelocity{0, 4, 0};
-
-  // Target state.
-  const Eigen::Vector3d kTargetPosition{0, 4, 0};
-  const Eigen::Vector3d kTargetVelocity{2, 2, -1};
-
   IdealSensorTest() : sensor_(IdealSensor(agent_)) {
+    const auto& agent_position = GetParam().agent_position;
+    const auto& agent_velocity = GetParam().agent_velocity;
     State agent_state;
-    agent_state.mutable_position()->set_x(kAgentPosition(0));
-    agent_state.mutable_position()->set_y(kAgentPosition(1));
-    agent_state.mutable_position()->set_z(kAgentPosition(2));
-    agent_state.mutable_velocity()->set_x(kAgentVelocity(0));
-    agent_state.mutable_velocity()->set_y(kAgentVelocity(1));
-    agent_state.mutable_velocity()->set_z(kAgentVelocity(2));
+    agent_state.mutable_position()->set_x(agent_position(0));
+    agent_state.mutable_position()->set_y(agent_position(1));
+    agent_state.mutable_position()->set_z(agent_position(2));
+    agent_state.mutable_velocity()->set_x(agent_velocity(0));
+    agent_state.mutable_velocity()->set_y(agent_velocity(1));
+    agent_state.mutable_velocity()->set_z(agent_velocity(2));
     agent_ = agent::ModelAgent(std::move(agent_state));
 
+    const auto& target_position = GetParam().target_position;
+    const auto& target_velocity = GetParam().target_velocity;
     State target_state;
-    target_state.mutable_position()->set_x(kTargetPosition(0));
-    target_state.mutable_position()->set_y(kTargetPosition(1));
-    target_state.mutable_position()->set_z(kTargetPosition(2));
-    target_state.mutable_velocity()->set_x(kTargetVelocity(0));
-    target_state.mutable_velocity()->set_y(kTargetVelocity(1));
-    target_state.mutable_velocity()->set_z(kTargetVelocity(2));
+    target_state.mutable_position()->set_x(target_position(0));
+    target_state.mutable_position()->set_y(target_position(1));
+    target_state.mutable_position()->set_z(target_position(2));
+    target_state.mutable_velocity()->set_x(target_velocity(0));
+    target_state.mutable_velocity()->set_y(target_velocity(1));
+    target_state.mutable_velocity()->set_z(target_velocity(2));
     target_ = agent::ModelAgent(std::move(target_state));
   }
 
@@ -54,50 +83,90 @@ class IdealSensorTest : public testing::Test {
   agent::ModelAgent target_;
 };
 
-TEST_F(IdealSensorTest, SensePositionRange) {
-  const auto expected_range = kTargetPosition.norm();
+TEST_P(IdealSensorTest, SensePositionRange) {
+  const auto expected_range = GetParam().expected_range;
   const auto sensor_output = sensor_.SensePosition(target_);
   EXPECT_NEAR(sensor_output.position().range(), expected_range,
               kMaxErrorTolerance);
 }
 
-TEST_F(IdealSensorTest, SensePositionAzimuth) {
-  const auto expected_azimuth =
-      std::atan(kTargetPosition(0) / kTargetPosition(1));
+TEST_P(IdealSensorTest, SensePositionAzimuth) {
+  const auto expected_azimuth = GetParam().expected_azimuth;
   const auto sensor_output = sensor_.SensePosition(target_);
   EXPECT_NEAR(sensor_output.position().azimuth(), expected_azimuth,
               kMaxErrorTolerance);
 }
 
-TEST_F(IdealSensorTest, SensePositionElevation) {
-  const auto expected_elevation = std::atan(
-      kTargetPosition(2) / std::sqrt(std::pow(kTargetPosition(0), 2) +
-                                     std::pow(kTargetPosition(1), 2)));
+TEST_P(IdealSensorTest, SensePositionElevation) {
+  const auto expected_elevation = GetParam().expected_elevation;
   const auto sensor_output = sensor_.SensePosition(target_);
   EXPECT_NEAR(sensor_output.position().elevation(), expected_elevation,
               kMaxErrorTolerance);
 }
 
-TEST_F(IdealSensorTest, SenseVelocityRange) {
-  const auto expected_range_rate = -2.0;
+TEST_P(IdealSensorTest, SenseVelocityRange) {
+  const auto expected_range_rate = GetParam().expected_range_rate;
   const auto sensor_output = sensor_.SenseVelocity(target_);
   EXPECT_NEAR(sensor_output.velocity().range(), expected_range_rate,
               kMaxErrorTolerance);
 }
 
-TEST_F(IdealSensorTest, SenseVelocityAzimuth) {
-  const auto expected_azimuth_velocity = 2.0 / 4;
+TEST_P(IdealSensorTest, SenseVelocityAzimuth) {
+  const auto expected_azimuth_velocity = GetParam().expected_azimuth_velocity;
   const auto sensor_output = sensor_.SenseVelocity(target_);
   EXPECT_NEAR(sensor_output.velocity().azimuth(), expected_azimuth_velocity,
               kMaxErrorTolerance);
 }
 
-TEST_F(IdealSensorTest, SenseVelocityElevation) {
-  const auto expected_elevation_velocity = -1.0 / 4;
+TEST_P(IdealSensorTest, SenseVelocityElevation) {
+  const auto expected_elevation_velocity =
+      GetParam().expected_elevation_velocity;
   const auto sensor_output = sensor_.SenseVelocity(target_);
   EXPECT_NEAR(sensor_output.velocity().elevation(), expected_elevation_velocity,
               kMaxErrorTolerance);
 }
+
+INSTANTIATE_TEST_SUITE_P(AzimuthElevation, IdealSensorTest,
+                         testing::Values(
+                             // Boresight.
+                             IdealSensorTestParam{
+                                 .agent_position = Eigen::Vector3d{0, 0, 0},
+                                 .agent_velocity = Eigen::Vector3d{0, 4, 0},
+                                 .target_position = Eigen::Vector3d{0, 4, 0},
+                                 .target_velocity = Eigen::Vector3d{2, 2, -1},
+                                 .expected_range = 4,
+                                 .expected_azimuth = 0,
+                                 .expected_elevation = 0,
+                                 .expected_range_rate = -2,
+                                 .expected_azimuth_velocity = 2.0 / 4,
+                                 .expected_elevation_velocity = -1.0 / 4,
+                             },
+                             // Starboard.
+                             IdealSensorTestParam{
+                                 .agent_position = Eigen::Vector3d{0, 0, 0},
+                                 .agent_velocity = Eigen::Vector3d{0, 1, 0},
+                                 .target_position = Eigen::Vector3d{5, 0, 0},
+                                 .target_velocity = Eigen::Vector3d{2, 3, -1},
+                                 .expected_range = 5,
+                                 .expected_azimuth = std::numbers::pi / 2,
+                                 .expected_elevation = 0,
+                                 .expected_range_rate = 2,
+                                 .expected_azimuth_velocity = -2.0 / 5,
+                                 .expected_elevation_velocity = -1.0 / 5,
+                             },
+                             // Above.
+                             IdealSensorTestParam{
+                                 .agent_position = Eigen::Vector3d{0, 0, 0},
+                                 .agent_velocity = Eigen::Vector3d{0, 1, 0},
+                                 .target_position = Eigen::Vector3d{0, 0, 5},
+                                 .target_velocity = Eigen::Vector3d{0, 2, 0},
+                                 .expected_range = 5,
+                                 .expected_azimuth = 0,
+                                 .expected_elevation = std::numbers::pi / 2,
+                                 .expected_range_rate = 0,
+                                 .expected_azimuth_velocity = 0,
+                                 .expected_elevation_velocity = -1.0 / 5,
+                             }));
 
 }  // namespace
 }  // namespace swarm::sensor
