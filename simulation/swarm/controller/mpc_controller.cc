3a3c1122122baa4caeff275fb90195ee58e4e672
Titan Yuan <tryuan99@gmail.com>
2024-10-09 00:11:36 -0700

    Add nonlinear MPC controller

--- simulation/swarm/controller/mpc_controller.cc
+++ simulation/swarm/controller/mpc_controller.cc
@@ -2,88 +2,156 @@
 
 #include <Eigen/Dense>
 
+#include "mpc/NLMPC.hpp"
 #include "simulation/swarm/controls/mpc_controller.h"
 #include "simulation/swarm/proto/sensor.pb.h"
 #include "simulation/swarm/utils/constants.h"
 
 namespace swarm::controller {
 
-void MpcController::PlanImpl(const SensorOutput& sensor_output) {
-  // Define helper variables.
-  const auto position = agent_->GetPosition();
-  const auto velocity = agent_->GetVelocity();
-  const auto normalized_velocity = velocity.normalized();
-  const auto acceleration = agent_->GetAcceleration();
+namespace {
+// Number of state variables.
+constexpr int kNumStateVariables = 7;
 
-  const auto g = constants::CalculateGravityAtAltitude(position(2));
-  const auto temp = std::sqrt(std::pow(acceleration.norm(), 2) +
-                              2 * g * acceleration(2) + std::pow(g, 2) -
-                              std::pow(acceleration.dot(normalized_velocity) +
-                                           g * normalized_velocity(2),
-                                       2));
+// Number of input variables.
+constexpr int kNumInputVariables = 3;
+
+// Position cost factor.
+constexpr double kPositionCostFactor = 2;
+
+// Prediction horizon in number of time steps.
+constexpr int kPredictionHorizon = 10;
+
+// Control horizon in number of time steps.
+constexpr int kControlHorizon = 5;
+
+// Sampling time in seconds.
+constexpr double kSamplingTime = 0.01;  // seconds
+
+// Number of inequality constraints.
+constexpr int kNumInequalityConstraints = kPredictionHorizon + 1;
+
+// Number of equality constraints.
+constexpr int kNumEqualityConstraints = kPredictionHorizon + 1;
+
+// Type aliases.
+using StateVector = Eigen::Vector<double, kNumStateVariables>;
+using StateMatrix =
+    Eigen::Matrix<double, kNumStateVariables, kNumStateVariables>;
+using InputVector = Eigen::Vector<double, kNumInputVariables>;
+}  // namespace
+
+void MpcController::PlanImpl(const SensorOutput& sensor_output) {
+  // Initialize the nonlinear model-predictive control controller.
+  mpc::NLMPC<kNumStateVariables, kNumInputVariables, kNumStateVariables,
+             kPredictionHorizon, kControlHorizon, kNumInequalityConstraints,
+             kNumEqualityConstraints>
+      controller;
+  controller.setDiscretizationSamplingTime(kSamplingTime);
+
+  mpc::NLParameters params;
+  params.maximum_iteration = 1000;
+  controller.setOptimizerParameters(params);
+
+  // Define the state equation.
+  controller.setStateSpaceFunction([&](StateVector& dx, const StateVector& x,
+                                       const InputVector& u,
+                                       const unsigned int& time_step) {
+    // Define helper variables.
+    const auto position = agent_->GetPosition();
+    const auto velocity = x.segment(3, 3);
+    const auto g = constants::CalculateGravityAtAltitude(position(2));
+    const auto rho = constants::CalculateAirDensityAtAltitude(position(2));
+
+    // Calculate the drag acceleration.
+    const auto air_drag_acceleration =
+        rho * agent_->static_config().lift_drag_config().drag_coefficient() *
+        agent_->static_config().body_config().cross_sectional_area() /
+        (2 * agent_->static_config().body_config().mass()) *
+        std::pow(velocity.norm(), 2);
+    const auto lift_induced_drag_acceleration =
+        (u + Eigen::Vector3d{0, 0, g}).norm() /
+        agent_->static_config().lift_drag_config().lift_drag_ratio();
+    const auto drag_acceleration =
+        air_drag_acceleration + lift_induced_drag_acceleration;
+
+    // Define the dx/dt vector.
+    dx.head(3) = x.segment(3, 3);
+    dx.segment(3, 3) = u - Eigen::Vector3d{0, 0, g} -
+                       drag_acceleration * velocity / velocity.norm();
+    dx(6) = drag_acceleration;
+  });
+
+  // Define the objective function.
+  controller.setObjectiveFunction(
+      [&](const Eigen::Matrix<double, kPredictionHorizon + 1,
+                              kNumStateVariables>& x,
+          const Eigen::Matrix<double, kPredictionHorizon + 1,
+                              kNumStateVariables>& y,
+          const Eigen::Matrix<double, kPredictionHorizon + 1,
+                              kNumInputVariables>& u,
+          const double& slack) {
+        return std::sqrt(
+            (x.row(x.rows() - 1).array() *
+             StateVector{std::sqrt(kPositionCostFactor),
+                         std::sqrt(kPositionCostFactor),
+                         std::sqrt(kPositionCostFactor), 0, 0, 0, 1}
+                 .transpose()
+                 .array())
+                .matrix()
+                .norm());
+      });
+
+  // Define the maximum acceleration inequality constraints.
+  controller.setIneqConFunction(
+      [&](Eigen::Vector<double, kNumInequalityConstraints>& inequalities,
+          const Eigen::Matrix<double, kPredictionHorizon + 1,
+                              kNumStateVariables>& x,
+          const Eigen::Matrix<double, kPredictionHorizon + 1,
+                              kNumStateVariables>& y,
+          const Eigen::Matrix<double, kPredictionHorizon + 1,
+                              kNumInputVariables>& u,
+          const double& slack) {
+        for (int i = 0; i < kNumInequalityConstraints; ++i) {
+          inequalities(i) =
+              u.row(i).norm() - std::pow(x.row(i).segment(3, 3).norm() /
+                                             agent_->static_config()
+                                                 .acceleration_config()
+                                                 .reference_speed(),
+                                         2) *
+                                    agent_->static_config()
+                                        .acceleration_config()
+                                        .max_reference_acceleration();
+        }
+      });
+
+  // Define the normal acceleration equality constraints.
+  controller.setEqConFunction(
+      [&](Eigen::Vector<double, kNumEqualityConstraints>& equalities,
+          const Eigen::Matrix<double, kPredictionHorizon + 1,
+                              kNumStateVariables>& x,
+          const Eigen::Matrix<double, kPredictionHorizon + 1,
+                              kNumInputVariables>& u) {
+        for (int i = 0; i < kNumEqualityConstraints; ++i) {
+          equalities(i) = u.row(i).dot(x.row(i).segment(3, 3));
+        }
+      });
 
   // Define the initial state vector.
-  Vector6d initial_state{sensor_output.position_cartesian().x(),
-                         sensor_output.position_cartesian().y(),
-                         sensor_output.position_cartesian().z(),
-                         velocity(0),
-                         velocity(1),
-                         velocity(2)};
+  const auto velocity = agent_->GetVelocity();
+  const auto normalized_velocity = velocity.normalized();
+  const StateVector initial_state{sensor_output.position_cartesian().x(),
+                                  sensor_output.position_cartesian().y(),
+                                  sensor_output.position_cartesian().z(),
+                                  velocity(0),
+                                  velocity(1),
+                                  velocity(2),
+                                  0};
 
-  // Define the A matrix.
-  Matrix6d A = Matrix6d::Zero();
-  A.block(0, 3, 3, 3) = Eigen::Matrix3d::Identity();
-  auto df_over_dv =
-      -(normalized_velocity.dot(acceleration) * Eigen::Matrix3d::Identity() +
-        normalized_velocity * acceleration.transpose() -
-        2 * normalized_velocity.dot(acceleration) * normalized_velocity *
-            normalized_velocity.transpose()) /
-          velocity.norm() -
-      constants::CalculateAirDensityAtAltitude(position(2)) *
-          agent_->static_config().lift_drag_config().drag_coefficient() *
-          agent_->static_config().body_config().cross_sectional_area() /
-          (2 * agent_->static_config().body_config().mass()) * velocity.norm() *
-          (Eigen::Matrix3d::Identity() +
-           normalized_velocity * normalized_velocity.transpose()) +
-      temp * (Eigen::Matrix3d::Identity() -
-              normalized_velocity * normalized_velocity.transpose()) +
-      (-(acceleration.dot(normalized_velocity) + g * normalized_velocity(2)) *
-           (acceleration + Eigen::Vector3d{0, 0, g}) +
-       std::pow(
-           (acceleration.dot(normalized_velocity) + g * normalized_velocity(2)),
-           2) *
-           normalized_velocity) *
-          normalized_velocity.transpose() /
-          (velocity.norm() *
-           agent_->static_config().lift_drag_config().lift_drag_ratio() * temp);
-  A.block(3, 3, 3, 3) = df_over_dv;
-
-  // Define the B matrix.
-  Eigen::Matrix<double, 6, 3> B = Eigen::Matrix<double, 6, 3>::Zero();
-  auto df_over_da =
-      Eigen::Matrix3d::Identity() -
-      normalized_velocity * normalized_velocity.transpose() +
-      normalized_velocity *
-          (acceleration.transpose() + Eigen::RowVector3d{0, 0, g} -
-           (acceleration.dot(normalized_velocity) +
-            g * normalized_velocity(2)) *
-               normalized_velocity.transpose()) /
-          (agent_->static_config().lift_drag_config().lift_drag_ratio() * temp);
-  B.block(3, 0, 3, 3) = df_over_da;
-
-  // Define the LQR cost matrices.
-  const auto Q = Matrix6d::Zero();
-  const auto R = kInputCostFactor * Eigen::Matrix3d::Identity();
-  Matrix6d Qf = Matrix6d::Identity();
-  Qf.block(0, 0, 3, 3) *= kPositionCostFactor;
-  Qf.block(3, 3, 3, 3) *= -1;
-
-  // Run the LQR solver.
-  controls::MpcController controller(A, B, kLqrSamplingTime, Q, R, Qf,
-                                     kLqrHorizon);
-  controller.Plan(initial_state);
-  const Eigen::Vector3d& acceleration_input =
-      controller.GetOptimalControl(acceleration);
+  // Run the optimizer.
+  mpc::Result<kNumInputVariables> result =
+      controller.optimize(initial_state, InputVector::Zero());
+  const Eigen::Vector3d& acceleration_input = result.cmd;
 
   // Extract the normal acceleration input only.
   acceleration_input_ =
--- simulation/swarm/controller/mpc_controller.h
+++ simulation/swarm/controller/mpc_controller.h
@@ -1,6 +1,5 @@
 // The model-predictive control controller controls the agent by optimizing the
-// trajectory with a linearized, receding finite horizon LQR as a feedback
-// policy.
+// trajectory over a receding finite horizon with nonlinear system dynamics.
 
 #pragma once
 
@@ -13,22 +12,6 @@ namespace swarm::controller {
 // Model-predictive control controller.
 class MpcController : public AgentController {
  public:
-  // Type aliases.
-  using Vector6d = Eigen::Vector<double, 6>;
-  using Matrix6d = Eigen::Matrix<double, 6, 6>;
-
-  // Position cost factor.
-  inline static const double kPositionCostFactor = 2;
-
-  // Input cost factor.
-  inline static const double kInputCostFactor = 1;
-
-  // Sampling time in seconds for the LQR solver.
-  inline static const double kLqrSamplingTime = 0.01;
-
-  // LQR horizon in number of time steps.
-  inline static const int kLqrHorizon = 100;
-
   MpcController(const agent::Agent& agent) : AgentController(agent) {}
 
   MpcController(MpcController&) = default;
