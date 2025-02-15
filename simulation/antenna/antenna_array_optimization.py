c21b4f21b07d507c50b8ad831373b0d24a6c9392
Titan Yuan <tryuan99@gmail.com>
2025-02-14 22:18:14 -0800

    Define antenna array optimization problems

---
+++ simulation/antenna/antenna_array_optimization.py
@@ -0,0 +1,392 @@
+"""The antenna array optimization problem finds the Pareto front between the
+main lobe width and the sidelobe level by formulating it as a multi-objective
+optimization problem.
+"""
+
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+from simulation.antenna.antenna_array import (AntennaArray,
+                                              AntennaArrayBeamSteer)
+from simulation.antenna.proto.antenna_array_config_pb2 import \
+    AntennaArrayConfig
+from simulation.antenna.proto.geometry_pb2 import (CartesianCoordinates,
+                                                   Quaternion)
+from utils.optimization.problem import Problem
+
+
+class Line(ABC):
+    """Interface for a line."""
+
+    @abstractmethod
+    def evaluate(self, x: float | np.ndarray) -> float | np.ndarray:
+        """Evaluates the y-coordinates corresponding to the given
+        x-coordinates.
+
+        Args:
+            x: x-coordinates.
+
+        Returns:
+            The y-coordinates corresponding to the given x-coordinates.
+        """
+
+
+class AntennaArrayOptimizationProblem(Problem):
+    """Antenna array optimization problem.
+
+    Attributes:
+        antenna_array_config: Antenna array configuration.
+    """
+
+    def __init__(self, antenna_array_config: AntennaArrayConfig) -> None:
+        self.antenna_array_config = antenna_array_config
+
+    def num_antenna_elements(self) -> int:
+        """Returns the number of antenna elements."""
+        return len(self.antenna_array_config.antenna_array_element_configs)
+
+    def num_objectives(self) -> int:
+        """Returns the number of objectives."""
+        # The two objectives are the main lobe width and the sidelobe level.
+        return 2
+
+    def evaluate_objectives(self, x: np.ndarray) -> list[float]:
+        """Evaluates the objective(s) on the given design variable values.
+
+        Args:
+            x: Design variable values.
+
+        Returns:
+            The objective(s) evaluated on the given design variable values.
+        """
+        positions = self._evaluate_positions(x)
+        orientations = self._evaluate_orientations(x)
+
+        # Copy the base antenna array configuration.
+        antenna_array_config = AntennaArrayConfig()
+        antenna_array_config.CopyFrom(self.antenna_array_config)
+
+        # Set the position and orientation for each antenna array element.
+        for i in range(self.num_antenna_elements()):
+            antenna_array_config.antenna_array_element_configs[
+                i].position = positions[i]
+            antenna_array_config.antenna_array_element_configs[
+                i].orientation = orientations[i]
+
+        # Evaluate the maximum main lobe width and the highest sidelobe level.
+        antenna_array = AntennaArray.create(antenna_array_config)
+        max_main_lobe_width = self._evaluate_max_main_lobe_width(antenna_array)
+        highest_sidelobe_level = self._evaluate_highest_sidelobe_level(
+            antenna_array)
+        return [max_main_lobe_width, highest_sidelobe_level]
+
+    @abstractmethod
+    def _evaluate_positions(self, x: np.ndarray) -> list[CartesianCoordinates]:
+        """Evaluates the positions of the antenna array elements.
+
+        Args:
+            x: Design variable values.
+
+        Returns:
+            The positions of the antenna array elements.
+        """
+
+    @abstractmethod
+    def _evaluate_orientations(self, x: np.ndarray) -> list[Quaternion]:
+        """Evaluates the orientations of the antenna array elements.
+
+        Args:
+            x: Design variable values.
+
+        Returns:
+            The orientations of the antenna array elements.
+        """
+
+    @abstractmethod
+    def _evaluate_max_main_lobe_width(
+        self,
+        antenna_array: AntennaArray,
+    ) -> float:
+        """Evaluates the maximum main lobe width of the antenna array.
+
+        Args:
+            antenna_array: Antenna array.
+
+        Returns:
+            The maximum main lobe width.
+        """
+
+    @abstractmethod
+    def _evaluate_highest_sidelobe_level(
+        self,
+        antenna_array: AntennaArray,
+    ) -> float:
+        """Evaluates the highest sidelobe level of the antenna array.
+
+        Args:
+            antenna_array: Antenna array.
+
+        Returns:
+            The highest sidelobe level.
+        """
+
+
+class AntennaArray1DOptimizationProblem(AntennaArrayOptimizationProblem):
+    """Antenna array 1D optimization problem."""
+
+    def __init__(self, antenna_array_config: AntennaArrayConfig) -> None:
+        super().__init__(antenna_array_config)
+
+    def _evaluate_max_main_lobe_width(
+        self,
+        antenna_array: AntennaArray,
+    ) -> float:
+        """Evaluates the maximum main lobe width of the antenna array.
+
+        Args:
+            antenna_array: Antenna array.
+
+        Returns:
+            The maximum main lobe width.
+        """
+        # For antenna arrays lying on a horizontal plane, fix the elevation at 0.
+        azimuth_sweep = np.linspace(-np.pi / 2, np.pi / 2, 180, endpoint=False)
+        azimuth_values = np.linspace(-np.pi, np.pi, 360, endpoint=False)
+
+        main_lobe_widths = np.zeros(azimuth_sweep.shape)
+        for azimuth_index, azimuth in enumerate(azimuth_sweep):
+            beam_steer = AntennaArrayBeamSteer(
+                azimuth=azimuth,
+                elevation=0,
+            )
+            radiation_pattern = antenna_array.calculate_radiation_pattern(
+                beam_steer,
+                azimuth=azimuth_values,
+                elevation=0,
+            )
+            azimuth_peak_index = np.argmin(np.abs(azimuth_values - azimuth))
+            main_lobe_width = (
+                radiation_pattern.main_lobe_width(azimuth_peak_index))
+            main_lobe_widths[azimuth_index] = main_lobe_width
+        return np.max(main_lobe_widths)
+
+    def _evaluate_highest_sidelobe_level(
+        self,
+        antenna_array: AntennaArray,
+    ) -> float:
+        """Evaluates the highest sidelobe level of the antenna array.
+
+        Args:
+            antenna_array: Antenna array.
+
+        Returns:
+            The highest sidelobe level.
+        """
+        # For antenna arrays lying on a horizontal plane, fix the elevation at 0.
+        azimuth_sweep = np.linspace(-np.pi / 2, np.pi / 2, 180, endpoint=False)
+        azimuth_values = np.linspace(-np.pi, np.pi, 360, endpoint=False)
+
+        sidelobe_levels = np.zeros(azimuth_sweep.shape)
+        for azimuth_index, azimuth in enumerate(azimuth_sweep):
+            beam_steer = AntennaArrayBeamSteer(
+                azimuth=azimuth,
+                elevation=0,
+            )
+            radiation_pattern = antenna_array.calculate_radiation_pattern(
+                beam_steer,
+                azimuth=azimuth_values,
+                elevation=0,
+            )
+            azimuth_peak_index = np.argmin(np.abs(azimuth_values - azimuth))
+            sidelobe_level = (
+                radiation_pattern.sidelobe_level(azimuth_peak_index))
+            sidelobe_levels[azimuth_index] = sidelobe_level
+        return np.min(sidelobe_levels)
+
+
+class AntennaArray1DApertureOptimizationProblem(
+        AntennaArray1DOptimizationProblem):
+    """Antenna array 1D aperture optimization problem.
+
+    The antenna array elements lie on a horizontal plane and are constrained to
+    a maximum aperture.
+
+    The design variables are ordered as follows:
+     - x-coordinate of antenna array element 0
+     - z-coordinate of antenna array element 0
+     - theta rotation aroudn the y-axis of antenna array element 0
+     - x-coordinate of antenna array element 1
+     - z-coordinate of antenna array element 1
+     - theta rotation aroudn the y-axis of antenna array element 1
+     - ...
+
+    Attributes:
+        x_min: Minimum x-coordinate.
+        x_max: Maximum x-coordinate.
+        z_min: Minimum z-coordinate.
+        z_max: Maximum z-coordinate.
+    """
+
+    def __init__(
+        self,
+        antenna_array_config: AntennaArrayConfig,
+        x_min: float,
+        x_max: float,
+        z_min: float,
+        z_max: float,
+    ) -> None:
+        super().__init__(antenna_array_config)
+        self.x_min = x_min
+        self.x_max = x_max
+        self.z_min = z_min
+        self.z_max = z_max
+
+    def num_variables_per_element(self) -> int:
+        """Returns the number of design variables per antenna array element."""
+        return 3
+
+    def num_variables(self) -> int:
+        """Returns the number of design variables."""
+        # Each antenna array element can be moved along the x and z-axes and
+        # rotated around the y-axis.
+        return self.num_antenna_elements() * self.num_variables_per_element()
+
+    def lower_bound(self) -> np.ndarray:
+        """Returns the lower bound on the design variables."""
+        lower_bound = np.array([self.x_min, self.z_min, -np.pi])
+        return np.tile(lower_bound, self.num_antenna_elements)
+
+    def upper_bound(self) -> np.ndarray:
+        """Returns the upper bound on the design variables."""
+        upper_bound = np.array([self.x_max, self.z_max, -np.pi])
+        return np.tile(upper_bound, self.num_antenna_elements)
+
+    def _evaluate_positions(self, x: np.ndarray) -> list[CartesianCoordinates]:
+        """Evaluates the positions of the antenna array elements.
+
+        Args:
+            x: Design variable values.
+
+        Returns:
+            The positions of the antenna array elements.
+        """
+        x_coordinates = x[::self.num_variables_per_element()]
+        z_coordinates = x[1::self.num_variables_per_element()]
+
+        # Set the position of each antenna array element.
+        positions = [
+            CartesianCoordinates() for _ in range(self.num_antenna_elements)
+        ]
+        for i in range(self.num_antenna_elements):
+            positions[i].x = x_coordinates[i]
+            positions[i].z = z_coordinates[i]
+        return positions
+
+    def _evaluate_orientations(self, x: np.ndarray) -> list[Quaternion]:
+        """Evaluates the orientations of the antenna array elements.
+
+        Args:
+            x: Design variable values.
+
+        Returns:
+            The orientations of the antenna array elements.
+        """
+        thetas = x[2::self.num_variables_per_element()]
+
+        # Set the orientation of each antenna array element.
+        orientations = [Quaternion() for _ in range(self.num_antenna_elements)]
+        for i in range(self.num_antenna_elements):
+            orientations[i].theta = thetas[i]
+            orientations[i].y = 1
+        return orientations
+
+
+class AntennaArray1DLineOptimizationProblem(AntennaArray1DOptimizationProblem):
+    """Antenna array 1D line optimization problem.
+
+    The antenna array elements are constrained to lie on a line.
+
+    The design variables are ordered as follows:
+     - x-coordinate of antenna array element 0
+     - theta rotation aroudn the y-axis of antenna array element 0
+     - x-coordinate of antenna array element 1
+     - theta rotation aroudn the y-axis of antenna array element 1
+     - ...
+
+    Attributes:
+        line: Line on which the antenna array elements lie.
+        x_min: Minimum x-coordinate.
+        x_max: Maximum x-coordinate.
+    """
+
+    def __init__(
+        self,
+        antenna_array_config: AntennaArrayConfig,
+        line: Line,
+        x_min: float,
+        x_max: float,
+    ) -> None:
+        super().__init__(antenna_array_config)
+        self.line = line
+        self.x_min = x_min
+        self.x_max = x_max
+
+    def num_variables_per_element(self) -> int:
+        """Returns the number of design variables per antenna array element."""
+        return 2
+
+    def num_variables(self) -> int:
+        """Returns the number of design variables."""
+        # Each antenna array element can be moved along the x-axis and rotated
+        # around the y-axis.
+        return self.num_antenna_elements() * self.num_variables_per_element()
+
+    def lower_bound(self) -> np.ndarray:
+        """Returns the lower bound on the design variables."""
+        lower_bound = np.array([self.x_min, -np.pi])
+        return np.tile(lower_bound, self.num_antenna_elements)
+
+    def upper_bound(self) -> np.ndarray:
+        """Returns the upper bound on the design variables."""
+        upper_bound = np.array([self.x_min, -np.pi])
+        return np.tile(upper_bound, self.num_antenna_elements)
+
+    def _evaluate_positions(self, x: np.ndarray) -> list[CartesianCoordinates]:
+        """Evaluates the positions of the antenna array elements.
+
+        Args:
+            x: Design variable values.
+
+        Returns:
+            The positions of the antenna array elements.
+        """
+        x_coordinates = x[::self.num_variables_per_element()]
+        z_coordinates = self.line.evaluate(x_coordinates)
+
+        # Set the position of each antenna array element.
+        positions = [
+            CartesianCoordinates() for _ in range(self.num_antenna_elements)
+        ]
+        for i in range(self.num_antenna_elements):
+            positions[i].x = x_coordinates[i]
+            positions[i].z = z_coordinates[i]
+        return positions
+
+    def _evaluate_orientations(self, x: np.ndarray) -> list[Quaternion]:
+        """Evaluates the orientations of the antenna array elements.
+
+        Args:
+            x: Design variable values.
+
+        Returns:
+            The orientations of the antenna array elements.
+        """
+        thetas = x[1::self.num_variables_per_element()]
+
+        # Set the orientation of each antenna array element.
+        orientations = [Quaternion() for _ in range(self.num_antenna_elements)]
+        for i in range(self.num_antenna_elements):
+            orientations[i].theta = thetas[i]
+            orientations[i].y = 1
+        return orientations
