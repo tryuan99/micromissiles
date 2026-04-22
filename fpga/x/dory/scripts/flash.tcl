bdd1f52dc1db12316f391ba7a77f6ad1a731793e
Titan Yuan <tryuan99@gmail.com>
2026-04-22 02:42:19 -0700

    Debug FPGA flash

--- fpga/x/dory/scripts/flash.tcl
+++ fpga/x/dory/scripts/flash.tcl
@@ -10,7 +10,7 @@ if {![file exists $bitfile]} {
     error "Bitstream not found: $bitfile"
 }
 
-# Create MCS.
+# Create MCS from the generated bitstream.
 write_cfgmem -force -format mcs -interface $cfgmem_interface -size $cfgmem_size_mb \
     -loadbit "up 0x0 $bitfile" $mcsfile
 
@@ -27,29 +27,40 @@ if {$device eq ""} {
 current_hw_device $device
 refresh_hw_device $device
 
-# Configure flash.
+# Reset any prior cfgmem association on the device.
 set existing_cfgmem [get_property PROGRAM.HW_CFGMEM $device]
 if {$existing_cfgmem ne ""} {
     delete_hw_cfgmem $existing_cfgmem
 }
 
+# Create and select the cfgmem object for this device.
 set cfgmem_part [lindex [get_cfgmem_parts $cfgmem_part_name] 0]
-set cfgmem [create_hw_cfgmem -hw_device $device -mem_dev $cfgmem_part]
+if {$cfgmem_part eq ""} {
+    error "No matching cfgmem part found for: $cfgmem_part_name"
+}
 
-# Set properties.
-set_property PROGRAM.FILES [list $mcsfile] $cfgmem
+create_hw_cfgmem -hw_device $device $cfgmem_part
+set cfgmem [current_hw_cfgmem -hw_device $device]
+
+# Configure the programming operation.
+set_property PROGRAM.FILE $mcsfile $cfgmem
 set_property PROGRAM.ADDRESS_RANGE {use_file} $cfgmem
-set_property PROGRAM.UNUSED_PIN_TERMINATION {pull-none} $cfgmem
-
-set_property PROGRAM.BLANK_CHECK 0 $cfgmem
+set_property PROGRAM.BLANK_CHECK 1 $cfgmem
 set_property PROGRAM.ERASE 1 $cfgmem
 set_property PROGRAM.CFG_PROGRAM 1 $cfgmem
 set_property PROGRAM.VERIFY 1 $cfgmem
 
+# Load Vivado's indirect programming bridge bitstream into the FPGA.
+set cfgmem_bitfile [get_property PROGRAM.HW_CFGMEM_BITFILE $device]
+if {$cfgmem_bitfile eq ""} {
+    error "Vivado did not provide a cfgmem bridge bitstream for $device"
+}
+
+create_hw_bitstream -hw_device $device $cfgmem_bitfile
+program_hw_devices $device
+
 # Program flash.
-startgroup
-program_hw_cfgmem -hw_cfgmem $cfgmem
-endgroup
+program_hw_cfgmem $cfgmem
 
 puts "Flash programming complete. Power-cycle or reset FPGA to load from flash."
 exit
